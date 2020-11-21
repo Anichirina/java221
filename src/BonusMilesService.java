9f87d7b6ff4531dbd24ed3a48f98ae9c0759c4ac
Irina Anichkova <anichkova2016@yandex.ru>
2020-11-21 20:31:57 +0300

    modified

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
     public long calculate(int price) {
-             part_ticket = 20;
-              miles = price / part_ticket;
-            return miles;
-        }
-                }
+        int partTicket = 20;
+        int miles = price / partTicket;
+        return miles;
+    }
+}
 
 
 
